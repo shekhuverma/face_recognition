dc1f77a1c3bba5ebdb0fc42d4d8848a6c92e7907
shekhuverma <shekharvrm47@gmail.com>
2017-06-14 21:21:47 +0530

    minor changes

--- data collector.py
+++ data collector.py
@@ -6,6 +6,10 @@ sampleNum=0
 while(True):
     ret, img = cam.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+##        preprocessing 
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(5,5))
+    cl1 = clahe.apply(gray)
+    gray=cv2.medianBlur(cl1,3)
     faces = detector.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
--- recognizer.py
+++ recognizer.py
@@ -1,8 +1,6 @@
 import cv2,time,matplotlib
 import numpy as np
-
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')
 recognizer = cv2.face.createLBPHFaceRecognizer()
 recognizer.load("data.yml")
 cam=cv2.VideoCapture(0)
@@ -15,11 +13,7 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 1)
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255),2)
-##        eyes=eye_cascade.detectMultiScale(gray[y:y+h,x:x+w])
-##        for (ex,ey,ew,eh) in eyes:
-##            cv2.rectangle(img[y:y+h,x:x+w],(ex,ey),(ex+ew,ey+eh),(255,0,255),2)
         Id,config=recognizer.predict(gray[y:y+h,x:x+w])
-        
         if config<75:
             if Id==1:
                 name="shekhar"
